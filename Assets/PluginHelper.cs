1c3bf159d8ce6acc81ce2159cfcae4e131016a22
James Andrew <jamesa@badvr.com>
2025-03-26 13:16:07 -0700

    removed unused methods

--- Assets/PluginHelper.cs
+++ Assets/PluginHelper.cs
@@ -8,12 +8,6 @@ public class PluginHelper : MonoBehaviour
     private static extern int _addTwoNumberInVisionOS(int a, int b);
 
     [DllImport("__Internal")]
-    private static extern void _setupDiscovery();
-
-    [DllImport("__Internal")]
-    private static extern void _startMonitoring();
-
-    [DllImport("__Internal")]
     private static extern int _getConnectedDeviceCount();
 
     [DllImport("__Internal")]
